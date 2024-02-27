09cc8aad3fcb36563f44e965c370a954c600add9
chethaka <csathmal24@gamil.com>
2024-02-27 19:57:19 +0530

    RTOD_main.py-initial-code-done

--- RTOD/RTOD_main.py
+++ RTOD/RTOD_main.py
@@ -1,7 +1,11 @@
+from datetime import datetime
 import os
+import platform
 import json
+import shutil
 
-def read_input(path_) -> dict:
+
+def read_input(path_: str) -> dict:
     '''read the content of json files and return it'''
 
     rtrn = None
@@ -11,8 +15,75 @@ def read_input(path_) -> dict:
 
     return rtrn
 
-def main():
-    pass
+def move_files(file_dict: dict) -> None:
+    '''make a main dir inside the specidied dir by the user
+    inside the main dir make a dir for each file type
+    move files into the dirs they belong'''
+
+    user_input = read_input(r"../RTOD_user_input.json")
+    info = user_input["type_and_location"] # dict
+    main_name = str(datetime.now())
+
+    for key, val in file_dict.items():
+        location = info[key]
+        main_path = os.path.join(location, main_name)
+        if not os.path.exists(main_path):
+            os.makedirs(main_path)
+        
+        sub_dir_name = key.upper()
+        sub_dir_path = os.path.join(main_path, sub_dir_name)
+        os.makedirs(sub_dir_path)
+
+        for file in val:
+            shutil.copy2(file.path, sub_dir_path)
+
+
+def organize_files(file_ls: list) -> None:
+    ''' organize files into dictionary based on their file extention '''
+    
+    file_dict = {}
+
+    for file in file_ls:
+        ext = file.name.split(".")[-1]
+        if ext not in file_dict.keys():
+            file_dict[ext] = []
+
+        file_dict[ext].append(file)
+    
+    move_files(file_dict)
+
+def get_files(path_: str, file_ls: list) -> None:
+    ''' load all the movable files in memory / python list'''
+    user_input = read_input(r"../RTOD_user_input.json")
+
+    for item in os.scandir(path_):
+        if item.is_dir() and os.listdir(item.path) != []:
+            get_files(path_, file_ls)
+        elif item.is_file() and item.name.split(".")[-1] in user_input["type_and_location"].keys():
+            file_ls.append(item)
+
+
+def main() -> None:
+    user_input = read_input(r"../RTOD_user_input.json")
+
+    if platform.system().lower() == 'windows':
+            # Get the absolute path of the user's profile directory
+            user_profile_dir = os.getenv('USERPROFILE')
+
+            # Join the profile directory with the standard "Downloads" directory name
+            download_dir = os.path.join(user_profile_dir, "Downloads")
+        
+    else:
+        # Get the absolute path of the user's home directory
+        home_dir = os.path.expanduser("~")
+
+        # Join the home directory with the standard "Downloads" directory name
+        download_dir = os.path.join(home_dir, "Downloads")
+
+    file_ls = []
+    get_files(download_dir, file_ls)
+
+    organize_files(file_ls)
 
 
 if __name__ == "__main__": 
