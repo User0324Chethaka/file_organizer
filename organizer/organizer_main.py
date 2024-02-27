e5956c2a5877cca43e63787be5f5ca3ffc5f03fb
chethaka <csathmal24@gamil.com>
2024-02-27 10:36:16 +0530

    organizer-works-now-with-0-errors

--- organizer/organizer_main.py
+++ organizer/organizer_main.py
@@ -4,16 +4,20 @@ import json
 import shutil
 
 
-def read_input(path) -> dict:
+def read_input(path_) -> dict:
+    '''read the content of json files and return it'''
+
     rtrn = None
-    with open(path, "r") as jf:
+    abs_path = os.path.abspath(path_)
+    with open(abs_path, "r") as jf:
         rtrn = json.load(jf)
 
     return rtrn
 
 
-def move_files_2(path_, category, value, time_):
-    # move files according to the structure 
+def move_files_2_back(path_, category, value, time_, *args):
+    ''' move files according to the structure  '''
+
     main_file_name = time_ 
     main_file_path = os.path.join(path_, main_file_name)
     if not os.path.exists(main_file_path):
@@ -28,74 +32,69 @@ def move_files_2(path_, category, value, time_):
         shutil.copy2(v.path, category_file_path)
 
 
-def move_files_1(file_dict):
-    # move files to either custom or default location
-    # user_input["save_location"] acts as default location
-    user_input = read_input(r"../../organizer_user_input.json")
+def move_files_1_back(file_dict, *args):
+    '''move files to either custom or default location
+       user_input["save_location"] acts as default location '''
+
+    user_input = read_input(r"../organizer_user_input.json")
     time_ = str(datetime.now())
     
     for category, value in file_dict.items():
         if category in user_input["custom"].keys():
-            move_files_2(user_input["custom"][category],
+            move_files_2_back(user_input["custom"][category],
                          category, 
                          value, 
                          time_)
         else:
-            move_files_2(user_input["save_location"],
+            move_files_2_back(user_input["save_location"],
                          category, 
                          value, 
                          time_)
     
 
 def organize_files(file_list):
-    # organize files into a dictionary
+    ''' organize files into a dictionary '''
+
     file_dict = {}
 
     for file in file_list:
         ext = file.name.split(".")[-1]
         if ext not in file_dict.keys():
             file_dict[ext] = []
-            file_dict[ext].append(file)
-        else:
-            file_dict[ext].append(file)
 
-    move_files_1(file_dict)
+        file_dict[ext].append(file)
+
+    move_files_1_back(file_dict)
 
 
-def get_files(path_, dont_move, all_files):
+def get_files(path_, all_files):
+    '''load all the files in the given file path into memory'''
+
+    user_input = read_input(r"../organizer_user_input.json") 
 
     for item in os.scandir(path=path_):
-        print(f'all files {all_files}')
-        print(f' item.name {item.name}', '\n\n\n')
-        if item.is_dir():
-            if os.listdir(item.path) != []: # chekc whether file is empty
-                get_files(item.path, dont_move, all_files)
-        else:
-            if item not in dont_move:
+
+        if item.name not in user_input["dont_move"]:
+            if item.is_dir() and os.listdir(item.path) != []:
+                get_files(path_=item.path, all_files=all_files)
+            elif not item.is_dir():
                 all_files.append(item)
     
-    #remove duplicates
-    user_input = read_input(r"../../organizer_user_input.json") 
-    if user_input["move_duplicates"].lower() == 'n':
-        all_files = set(all_files)
-
-    organize_files(all_files)
-
 
 def main():
-    user_input = read_input(r"../../organizer_user_input.json") 
-    file_paths = list(user_input["file_locations"])
-    print(f'file_path {file_paths}')
-    
-    # load all the files that need to be moved to a list
-    all_files = [] 
+    user_input = read_input(r"../organizer_user_input.json") 
+    src_file_paths = list(user_input["file_locations"])
 
-    for fp in file_paths:
-        print(f'fp {fp}')
-        print(f'os.listdir(fp) {os.listdir(fp)}')
+    for fp in src_file_paths:
+
+        # load all the files that need to be moved to a list
+        all_files = [] 
+
         # check if the dir is empty
         if os.listdir(fp) != []:
-            get_files(fp, user_input["dont_move"], all_files)
+            get_files(path_=fp, all_files=all_files)
+
+        organize_files(all_files)
 
         if user_input["move_and_delete"].lower() == 'y':
             os.rmdir(fp)
