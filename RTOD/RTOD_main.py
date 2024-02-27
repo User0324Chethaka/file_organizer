8eca2501ea397294dbd661e31b66864d4e3ad8ab
chethaka <csathmal24@gamil.com>
2024-02-27 13:52:29 +0530

    jfdk

--- RTOD/RTOD_main.py
+++ RTOD/RTOD_main.py
@@ -1,8 +1,15 @@
 import os
 import json
 
-def extract_uer_input():
-    file_path = r""
+def read_input(path_) -> dict:
+    '''read the content of json files and return it'''
+
+    rtrn = None
+    abs_path = os.path.abspath(path_) # get the absolute path
+    with open(abs_path, "r") as jf:
+        rtrn = json.load(jf)
+
+    return rtrn
 
 def main():
     pass
